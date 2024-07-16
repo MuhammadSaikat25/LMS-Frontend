f3189845a1cb616c1225657d958eff9fe1f9c19f
Muhammad Saikat <smsaikat000@gmail.com>
2024-07-16 19:44:10 +0600

    setup commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,4 @@
+import { Poppins } from "next/font/google";
 import type { Config } from "tailwindcss";
 
 const config: Config = {
@@ -8,10 +9,22 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        Poppins: ["var(--front-Poppins)"],
+        Josefin: ["var(--front-Josefin)"],
+      },
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-radial": "radial-gradient(var--tw-gradient-stops)",
         "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+          "conic-gradient(from 180deg at 50% 50%,var(--tw-gradient-stops))",
+      },
+      screens: {
+        "1000px": "1000px",
+        "1100px": "1100px",
+        "1200px": "1200px",
+        "1300px": "1300px",
+        "800px": "800px",
+        "400px": "400px",
       },
     },
   },
